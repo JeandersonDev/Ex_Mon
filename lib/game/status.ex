789b371206f35ba49ab889f5181905a64a1cac76
JeandersonDev <jeanderson.2315@gmail.com>
2022-07-22 16:54:40 -0300

    Update status and info

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,12 +1,15 @@
 defmodule ExMon.Game.Status do
   alias ExMon.Game
   def print_round_message() do
-    IO.puts("\n ===== Hi #{Game.player.name} The game is started! =====\n")
+    IO.puts("\n ===== Olá #{Game.player.name} O Jogo iniciou! =====\n")
     IO.inspect(Game.info())
     IO.puts("------------------------------------------")
   end
   def print_wrong_move_message(move) do
-    IO.puts("\n ===== Invalid move: #{move}. =====\n")
+    IO.puts("\n ===== Movimento Inválido: #{move}. =====\n")
   end
 
+  def print_move_message(:computer, :attack, damage, move) do
+    IO.puts("\n ===== #{Game.player.name} atacou o computador com #{move} tirando #{damage}% de vida. =====\n")
+  end
 end
