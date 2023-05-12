f207704d9bea58e080d7402d646919d603b58a57
Jeanderson Almeida <jeanderson.2315@gmail.com>
2023-05-12 11:30:44 -0300

    create function game_over

--- lib/game/status.ex
+++ lib/game/status.ex
@@ -1,36 +1,46 @@
 defmodule ExMon.Game.Status do
   alias ExMon.Game
-  alias ExMon.Game.Actions
-  def print_round_message(%{status: :started} =info ) do
-    IO.puts("\n ===== Ola #{Game.player.name} O Jogo iniciou! =====\n")
+
+  def print_round_message(%{status: :started} = info) do
+    IO.puts("\n ===== Ola #{Game.player().name} O Jogo iniciou! =====\n")
     IO.inspect(info)
     IO.puts("------------------------------------------")
   end
-  def print_round_message(%{status: :continue, turn: player} =info ) do
+
+  def print_round_message(%{status: :continue, turn: player} = info) do
     IO.puts("\n ===== Eh a vez de #{player} jogar! =====\n")
     IO.inspect(info)
     IO.puts("------------------------------------------")
   end
-  def print_round_message(%{status: :game_over} =info ) do
+
+  def print_round_message(%{status: :game_over} = info) do
     IO.puts("\n ===== Fim de Jogo ! =====\n")
     IO.inspect(info)
     IO.puts("------------------------------------------")
   end
+
   @spec print_wrong_move_message(any) :: :ok
   def print_wrong_move_message(move) do
     IO.puts("\n ===== Movimento Inválido: #{move}. =====\n")
   end
 
   def print_move_message(:computer, :attack, damage) do
-    IO.puts("\n ===== #{Game.player.name} atacou #{Game.computer.name} tirando #{damage}% de vida. =====\n")
+    IO.puts(
+      "\n ===== #{Game.player().name} atacou #{Game.computer().name} tirando #{damage}% de vida. =====\n"
+    )
   end
+
   def print_move_message(:player, :attack, damage) do
-    IO.puts("\n ===== #{Game.computer.name} atacou #{Game.player.name} tirando #{damage}% de vida. =====\n")
+    IO.puts(
+      "\n ===== #{Game.computer().name} atacou #{Game.player().name} tirando #{damage}% de vida. =====\n"
+    )
   end
+
   def print_move_message(:player, :heal, life) do
-    IO.puts("\n ===== #{Game.player.name} se curou e está com #{life}% de vida. =====\n")
+    IO.puts("\n ===== #{Game.player().name} se curou e está com #{life}% de vida. =====\n")
   end
-  def print_move_message(player, :heal, life) do
-    IO.puts("\n ===== #{Game.player.computer} se curou e está com #{life}% de vida. =====\n")
+
+  def print_move_message(:computer, :heal, life) do
+    IO.puts("\n ===== #{Game.computer().name} se curou e está com #{life}% de vida. =====\n")
   end
 end
